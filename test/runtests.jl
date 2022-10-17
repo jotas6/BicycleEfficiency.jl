788783512268b62eed85e14144ef7171bba66548
Juan José González Oneto <jj.gonzalez20@uniandes.edu.co>
2022-10-17 12:09:42 -0500

    Added test for efficiency at SpicerFriction

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,4 +12,5 @@ N = [50, 21]
     @test P3(μ, T0, rR, N, ω1, ψ) ≈ 0.022528056986976294 # Test for P3
     @test Ptotal(fill(μ, 3), p, ρ, ψ, rR, T0, N, ω1, γ) ≈ 3.0192098086672017 # Test for total power loss
     @test P1(μ, ρ, T0, N, ω1) + P2(N, ω1, μ, T0, rR, γ) + P3(μ, T0, rR, N, ω1, ψ) == Ptotal(fill(μ, 3), p, ρ, ψ, rR, T0, N, ω1, γ)
+    @test η(fill(μ, 3), p, ρ, ψ, rR, T0, N, ω1, γ) ≈ 0.9802343056715731 # Test for efficiency calculation
 end
