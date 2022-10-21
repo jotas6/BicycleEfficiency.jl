8bc19efc079571a0386e32b8a436cb0615ca509e
Juan José González Oneto <jj.gonzalez20@uniandes.edu.co>
2022-10-21 10:46:04 -0500

    Added documentation
    Created initial files to support documentation

---
+++ docs/make.jl
@@ -0,0 +1,4 @@
+using Documenter
+using BicycleEfficiency
+
+makedocs(sitename = "BicycleEfficiency.jl")
---
+++ docs/src/index.md
@@ -0,0 +1,10 @@
+# Documentation
+
+## Frictional losses
+```@docs
+P1
+P2
+P3
+Ptotal
+η
+```
