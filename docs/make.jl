b636b056406ea2555236d9dda014aa3d2dfb3556
Juan José González Oneto <jj.gonzalez20@uniandes.edu.co>
2022-10-22 21:35:29 -0500

    Fixed documentation issue
    Attempt # 3

--- docs/make.jl
+++ docs/make.jl
@@ -2,3 +2,8 @@ using Documenter
 using BicycleEfficiency
 
 makedocs(sitename = "BicycleEfficiency.jl")
+
+deploydocs(
+    repo = "github.com/jotas6/BicycleEfficiency.jl.git",
+    versions = nothing
+)
