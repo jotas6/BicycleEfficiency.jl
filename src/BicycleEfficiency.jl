e1e7ed0695505017cf77c332cff29c02b167d062
Juan José González Oneto <jj.gonzalez20@uniandes.edu.co>
2022-11-05 12:19:37 -0500

    Allowed public use of functions that calculate chain tension

--- src/BicycleEfficiency.jl
+++ src/BicycleEfficiency.jl
@@ -3,7 +3,8 @@ module BicycleEfficiency
 # Write your package code here.
 
 include("SpicerFriction.jl")
+include("tension.jl")
 
-export P1, P2, P3, Ptotal, η, P1T, P2T, P3T, PtotalT, ηT
+export P1, P2, P3, Ptotal, η, P1T, P2T, P3T, PtotalT, ηT, Tf, Tc, T
 
 end
